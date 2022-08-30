0e8cc66bbfcb7b799dc2317393518854bede5330
Luke <luke.c.dev@gmail.com>
2022-08-30 22:58:31 +0200

    updated with astronvim

--- zsh/env.zsh
+++ zsh/env.zsh
@@ -1,3 +1,6 @@
 #!/bin/zsh
 # setopt auto_cd
-cdpath=($HOME $HOME/.config $HOME/devops/ds_project_work $HOME/edgerx $HOME/edgerx/rust $HOME/devops/cicd $HOME/sre/monitoring $HOME/sre)
+cdpath=($HOME $HOME/.config $HOME/devops/ds_project_work $HOME/edgerx $HOME/edgerx/rust $HOME/devops/cicd $HOME/sre/monitoring $HOME/sre $HOME/devops ..)
+
+setopt hist_ignore_all_dups
+setopt hist_ignore_space
